fc998621293300a31cdd28493640d000a356002d
Xiaoen Ju <lepsognema@gmail.com>
2023-08-08 19:28:10 +0000

    reformat vamana/neighbors.h

--- algorithms/vamana/neighbors.h
+++ algorithms/vamana/neighbors.h
@@ -21,73 +21,77 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <algorithm>
+
+#include "../utils/NSGDist.h"
+#include "../utils/beamSearch.h"
+#include "../utils/check_nn_recall.h"
+#include "../utils/indexTools.h"
+#include "../utils/parse_results.h"
+#include "../utils/stats.h"
+#include "../utils/types.h"
+#include "index.h"
 #include "parlay/parallel.h"
 #include "parlay/primitives.h"
 #include "parlay/random.h"
-#include "../utils/NSGDist.h"
-#include "../utils/types.h"
-#include "index.h"
-#include "../utils/beamSearch.h"
-#include "../utils/indexTools.h"
-#include "../utils/stats.h"
-#include "../utils/parse_results.h"
-#include "../utils/check_nn_recall.h"
 
 extern bool report_stats;
 
-template<typename T>
-void ANN(parlay::sequence<Tvec_point<T>*> &v, int k, int maxDeg,
-	 int beamSize, int beamSizeQ, double alpha, double dummy,
-	 parlay::sequence<Tvec_point<T>*> &q,
-	 parlay::sequence<ivec_point> &groundTruth, char* res_file, bool graph_built, Distance* D) {
-  parlay::internal::timer t("ANN",report_stats);
+template <typename T>
+void ANN(parlay::sequence<Tvec_point<T> *> &v, int k, int maxDeg, int beamSize,
+         int beamSizeQ, double alpha, double dummy,
+         parlay::sequence<Tvec_point<T> *> &q,
+         parlay::sequence<ivec_point> &groundTruth, char *res_file,
+         bool graph_built, Distance *D) {
+  parlay::internal::timer t("ANN", report_stats);
   unsigned d = (v[0]->coordinates).size();
   using findex = knn_index<T>;
   findex I(maxDeg, beamSize, alpha, d, D);
   double idx_time;
-  if(graph_built){
+  if (graph_built) {
     I.find_approx_medoid(v);
     idx_time = 0;
-  } else{
-    parlay::sequence<int> inserts = parlay::tabulate(v.size(), [&] (size_t i){
-					    return static_cast<int>(i);});
+  } else {
+    parlay::sequence<int> inserts = parlay::tabulate(
+        v.size(), [&](size_t i) { return static_cast<int>(i); });
     I.build_index(v, std::move(inserts));
     idx_time = t.next_time();
   }
 
   int medoid = I.get_medoid();
   std::string name = "Vamana";
-  std::string params = "R = " + std::to_string(maxDeg) + ", L = " + std::to_string(beamSize);
+  std::string params =
+      "R = " + std::to_string(maxDeg) + ", L = " + std::to_string(beamSize);
   auto [avg_deg, max_deg] = graph_stats(v);
   auto vv = visited_stats(v);
-  std::cout << "Average visited: " << vv[0] << ", Tail visited: " << vv[1] << std::endl;
+  std::cout << "Average visited: " << vv[0] << ", Tail visited: " << vv[1]
+            << std::endl;
   Graph G(name, params, v.size(), avg_deg, max_deg, idx_time);
   G.print();
   search_and_parse(G, v, q, groundTruth, res_file, D, false, medoid);
-  
 }
 
-
-template<typename T>
-void ANN(parlay::sequence<Tvec_point<T>*> &v, int maxDeg, int beamSize, double alpha, double dummy, bool graph_built, Distance* D) {
-  parlay::internal::timer t("ANN",report_stats);
+template <typename T>
+void ANN(parlay::sequence<Tvec_point<T> *> &v, int maxDeg, int beamSize,
+         double alpha, double dummy, bool graph_built, Distance *D) {
+  parlay::internal::timer t("ANN", report_stats);
   {
     unsigned d = (v[0]->coordinates).size();
     using findex = knn_index<T>;
     findex I(maxDeg, beamSize, alpha, d, D);
-    if(graph_built) I.find_approx_medoid(v);
-    else{
-      parlay::sequence<int> inserts = parlay::tabulate(v.size(), [&] (size_t i){
-					    return static_cast<int>(i);});
+    if (graph_built)
+      I.find_approx_medoid(v);
+    else {
+      parlay::sequence<int> inserts = parlay::tabulate(
+          v.size(), [&](size_t i) { return static_cast<int>(i); });
       I.build_index(v, std::move(inserts));
       t.next("Built index");
     }
 
-    if(report_stats){
+    if (report_stats) {
       auto [avg_deg, max_deg] = graph_stats(v);
-      std::cout << "Index built with average degree " << avg_deg << " and max degree " << max_deg << std::endl;
+      std::cout << "Index built with average degree " << avg_deg
+                << " and max degree " << max_deg << std::endl;
       t.next("stats");
     }
   };
 }
-
