9d6812449ddcc94a81a1f65538ebda00ce010e3a
Ben Landrum <8enlandrum@gmail.com>
2023-09-13 11:34:57 -0400

    easier test file

--- python/test.py
+++ python/test.py
@@ -1,7 +1,16 @@
 import _ParlayANNpy as pann
+import wrapper as wp
 
-# pann.build_vamana_uint8_euclidian_index("Euclidian", "/ssd1/data/bigann/base.1B.u8bin.crop_nb_1000000", "/ssd1/data/bigann/outputs/parlayann", 64, 128, 1.2)
+print(dir(pann))
 
-Index = pann.VamanaUInt8EuclidianIndex("/ssd1/data/bigann/base.1B.u8bin.crop_nb_1000000", "/ssd1/data/bigann/outputs/parlayann", 1000000, 128)
-neighbors, distances = Index.batch_search_from_string("/ssd1/data/bigann/query.public.10K.u8bin", 10000, 10, 100)
-Index.check_recall("/ssd1/data/bigann/bigann-1M", neighbors, 10)
+FERN_DATA_DIR = "/ssd1/anndata/bigann/"
+AWARE_DATA_DIR = "/ssd1/data/bigann/"
+
+DATA_DIR = FERN_DATA_DIR
+
+wp.build_vamana_index("Euclidian", "uint8", DATA_DIR + "base.1B.u8bin.crop_nb_1000000", DATA_DIR + "outputs/parlayann", 64, 128, 1.2)
+
+Index = wp.load_vamana_index("Euclidian", "uint8", DATA_DIR + "base.1B.u8bin.crop_nb_1000000", DATA_DIR + "outputs/parlayann", 1000000, 128)
+neighbors, distances = Index.batch_search_from_string(DATA_DIR + "query.public.10K.u8bin", 10000, 10, 100)
+
+# Index.check_recall(DATA_DIR + "bigann-1M", neighbors, 10)
