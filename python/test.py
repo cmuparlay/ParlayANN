382dad2e123ec74d730289ec8c77b7fb9abd8f03
Ben Landrum <8enlandrum@gmail.com>
2023-09-14 11:31:43 -0400

    add numpy query to test.py

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 import _ParlayANNpy as pann
 import wrapper as wp
+import numpy as np
 
 print(dir(pann))
 
@@ -8,9 +9,15 @@ AWARE_DATA_DIR = "/ssd1/data/bigann/"
 
 DATA_DIR = FERN_DATA_DIR
 
-wp.build_vamana_index("Euclidian", "uint8", DATA_DIR + "base.1B.u8bin.crop_nb_1000000", DATA_DIR + "outputs/parlayann", 64, 128, 1.2)
+# wp.build_vamana_index("Euclidian", "uint8", DATA_DIR + "base.1B.u8bin.crop_nb_1000000", DATA_DIR + "outputs/parlayann", 64, 128, 1.2)
 
 Index = wp.load_vamana_index("Euclidian", "uint8", DATA_DIR + "base.1B.u8bin.crop_nb_1000000", DATA_DIR + "outputs/parlayann", 1000000, 128)
-neighbors, distances = Index.batch_search_from_string(DATA_DIR + "query.public.10K.u8bin", 10000, 10, 100)
 
+# neighbors, distances = Index.batch_search_from_string(DATA_DIR + "query.public.10K.u8bin", 10000, 10, 100)
+
+query = np.fromfile(DATA_DIR + "query.public.10K.u8bin", dtype=np.uint8)[8:].reshape((10000, 128))
+neighbors, distances = Index.batch_search(query, 10000, 10, 100)
+
+print(neighbors.shape)
+print(neighbors[:10, :])
 # Index.check_recall(DATA_DIR + "bigann-1M", neighbors, 10)
