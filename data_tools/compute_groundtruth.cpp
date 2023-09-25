fb84391bcab1ea376537755c65d07ec3e44eb2f1
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-09-25 12:04:22 -0400

    compute gt updated

--- data_tools/compute_groundtruth.cpp
+++ data_tools/compute_groundtruth.cpp
@@ -3,25 +3,27 @@
 #include "parlay/parallel.h"
 #include "parlay/primitives.h"
 #include "parlay/io.h"
-#include "utils/types.h"
-#include "utils/NSGDist.h"
-#include "utils/parse_files.h"
+// #include "utils/types.h"
+#include "utils/euclidian_point.h"
+#include "utils/mips_point.h"
+#include "utils/point_range.h"
+
 
 using pid = std::pair<int, float>;
 
-template<typename T>
-parlay::sequence<parlay::sequence<pid>> compute_groundtruth(parlay::sequence<Tvec_point<T>> &B, 
-  parlay::sequence<Tvec_point<T>> &Q, int k, Distance* D){
-    unsigned d = (B[0].coordinates).size();
+template<typename PointRange>
+parlay::sequence<parlay::sequence<pid>> compute_groundtruth(PointRange &B, 
+  PointRange &Q, int k){
+    unsigned d = B.dimension();
     size_t q = Q.size();
     size_t b = B.size();
     auto answers = parlay::tabulate(q, [&] (size_t i){  
-        float topdist = 0;
-        if(D->id()=="mips") topdist = -std::numeric_limits<float>::max();     
+        float topdist = B[0].d_max();   
         int toppos;
         parlay::sequence<pid> topk;
         for(size_t j=0; j<b; j++){
-            float dist = D->distance((Q[i].coordinates).begin(), (B[j].coordinates).begin(), d);
+            // float dist = D->distance((Q[i].coordinates).begin(), (B[j].coordinates).begin(), d);
+            float dist = Q[i].distance(B[j]);
             if(topk.size() < k){
                 if(dist > topdist){
                     topdist = dist;   
@@ -30,8 +32,7 @@ parlay::sequence<parlay::sequence<pid>> compute_groundtruth(parlay::sequence<Tve
                 topk.push_back(std::make_pair((int) j, dist));
             }
             else if(dist < topdist){
-                float new_topdist=0;
-                if(D->id()=="mips") new_topdist = -std::numeric_limits<float>::max();   
+                float new_topdist=B[0].d_max();  
                 int new_toppos=0;
                 topk[toppos] = std::make_pair((int) j, dist);
                 for(size_t l=0; l<topk.size(); l++){
@@ -50,32 +51,7 @@ parlay::sequence<parlay::sequence<pid>> compute_groundtruth(parlay::sequence<Tve
     return answers;
 }
 
-void write_ivecs(parlay::sequence<parlay::sequence<pid>> &result, const std::string outFile, int k){
-    std::cout << "Writing file with dimension " << result[0].size() << std::endl;
-    std::cout << "File contains groundtruth for " << result.size() << " data points" << std::endl;
 
-    auto less = [&] (pid a, pid b) {return a.second < b.second;};
-
-    size_t n = result.size();
-    auto vects = parlay::tabulate(result.size(), [&] (size_t i){
-        parlay::sequence<int> data;
-        data.push_back(k);
-
-        auto sorted = parlay::sort(result[i], less);
-        for(int j=0; j<k; j++){
-          data.push_back(sorted[j].first);
-        }
-        return data;
-    });
-
-    parlay::sequence<int> to_write = parlay::flatten(vects);
-
-    auto data = to_write.begin();
-    std::ofstream writer;
-    writer.open(outFile, std::ios::binary | std::ios::out);
-    writer.write((char *) data, n * (k+1) * sizeof(int));
-    writer.close();
-}
 
 void write_ibin(parlay::sequence<parlay::sequence<pid>> &result, const std::string outFile, int k){
     std::cout << "Writing file with dimension " << result[0].size() << std::endl;
@@ -118,42 +94,25 @@ void write_ibin(parlay::sequence<parlay::sequence<pid>> &result, const std::stri
 
 int main(int argc, char* argv[]) {
   commandLine P(argc,argv,
-  "[-base_path <b>] [-query_path <q>] [-file_type <f>]"
+  "[-base_path <b>] [-query_path <q>] "
       "[-data_type <d>] [-k <k> ] [-dist_func <d>] [-gt_path <outfile>]");
 
   char* gFile = P.getOptionValue("-gt_path");
   char* qFile = P.getOptionValue("-query_path");
   char* bFile = P.getOptionValue("-base_path");
-  char* filetype = P.getOptionValue("-file_type");
   char* vectype = P.getOptionValue("-data_type");
   char* dfc = P.getOptionValue("-dist_func");
   int k = P.getOptionIntValue("-k", 100);
 
   std::string df = std::string(dfc);
-  Distance* D;
-  if(df == "Euclidian") D = new Euclidian_Distance();
-  else if(df == "mips") D = new Mips_Distance();
-  else{
-    std::cout << "Error: invalid distance type" << std::endl;
+  if(df != "Euclidian" && df != "mips"){
+    std::cout << "Error: invalid distance type: specify Euclidian or mips" << std::endl;
     abort();
   }
 
-  std::string ft = std::string(filetype);
   std::string tp = std::string(vectype);
-
-  
-  if((ft != "bin") && (ft != "vec")){
-    std::cout << "Error: file type not specified correctly, specify bin or vec" << std::endl;
-    abort();
-  }
-
   if((tp != "uint8") && (tp != "int8") && (tp != "float")){
-    std::cout << "Error: vector type not specified correctly, specify int8, uint8, or float" << std::endl;
-    abort();
-  }
-
-  if((ft == "vec") && (tp == "int8")){
-    std::cout << "Error: incompatible file and vector types" << std::endl;
+    std::cout << "Error: data type not specified correctly, specify int8, uint8, or float" << std::endl;
     abort();
   }
 
@@ -165,136 +124,55 @@ int main(int argc, char* argv[]) {
   std::string base = std::string(bFile);
   std::string query = std::string(qFile);
 
-  if(ft == "vec"){
-    if(tp == "float"){
-      std::cout << "Detected float coordinates" << std::endl;
-      auto [md, B] = parse_fvecs(bFile, NULL, maxDeg);
-      auto [fd, Q] = parse_fvecs(qFile, NULL, maxDeg);
+
+  if(tp == "float"){
+    std::cout << "Detected float coordinates" << std::endl;
+    if(df == "Euclidian"){
+      PointRange<float, Euclidian_Point<float>> B = PointRange<float, Euclidian_Point<float>>(bFile);
+      PointRange<float, Euclidian_Point<float>> Q = PointRange<float, Euclidian_Point<float>>(qFile);
       std::cout << "Base file size " << B.size() << std::endl;
       std::cout << "Query file size " << Q.size() << std::endl;
-      answers = compute_groundtruth<float>(B, Q, k, D);
-    }else if(tp == "uint8"){
-      std::cout << "Detected uint8 coordinates" << std::endl;
-      auto [md, B] = parse_bvecs(bFile, NULL, maxDeg);
-      auto [fd, Q] = parse_bvecs(qFile, NULL, maxDeg);
+      answers = compute_groundtruth<PointRange<float, Euclidian_Point<float>>>(B, Q, k);
+    } else if(df == "mips"){
+      PointRange<float, Mips_Point<float>> B = PointRange<float, Mips_Point<float>>(bFile);
+      PointRange<float, Mips_Point<float>> Q = PointRange<float, Mips_Point<float>>(qFile);
       std::cout << "Base file size " << B.size() << std::endl;
       std::cout << "Query file size " << Q.size() << std::endl;
-      answers = compute_groundtruth<uint8_t>(B, Q, k, D);
+      answers = compute_groundtruth<PointRange<float, Mips_Point<float>>>(B, Q, k);
     }
-    write_ivecs(answers, std::string(gFile), k);
-  } else if(ft == "bin"){
-    if(tp == "float"){
-      std::cout << "Detected float coordinates" << std::endl;
-      auto [md, B] = parse_fbin(bFile, NULL, maxDeg);
-      auto [fd, Q] = parse_fbin(qFile, NULL, maxDeg);
+  }else if(tp == "uint8"){
+    std::cout << "Detected uint8 coordinates" << std::endl;
+    if(df == "Euclidian"){
+      PointRange<uint8_t, Euclidian_Point<uint8_t>> B = PointRange<uint8_t, Euclidian_Point<uint8_t>>(bFile);
+      PointRange<uint8_t, Euclidian_Point<uint8_t>> Q = PointRange<uint8_t, Euclidian_Point<uint8_t>>(qFile);
       std::cout << "Base file size " << B.size() << std::endl;
       std::cout << "Query file size " << Q.size() << std::endl;
-      answers = compute_groundtruth<float>(B, Q, k, D);
-    }else if(tp == "uint8"){
-      std::cout << "Detected uint8 coordinates" << std::endl;
-      auto [md, B] = parse_uint8bin(bFile, NULL, maxDeg);
-      auto [fd, Q] = parse_uint8bin(qFile, NULL, maxDeg);
+      answers = compute_groundtruth<PointRange<uint8_t, Euclidian_Point<uint8_t>>>(B, Q, k);
+    } else if(df == "mips"){
+      PointRange<uint8_t, Mips_Point<uint8_t>> B = PointRange<uint8_t, Mips_Point<uint8_t>>(bFile);
+      PointRange<uint8_t, Mips_Point<uint8_t>> Q = PointRange<uint8_t, Mips_Point<uint8_t>>(qFile);
       std::cout << "Base file size " << B.size() << std::endl;
       std::cout << "Query file size " << Q.size() << std::endl;
-      answers = compute_groundtruth<uint8_t>(B, Q, k, D);
-    }else if(tp == "int8"){
-      std::cout << "Detected int8 coordinates" << std::endl;
-      auto [md, B] = parse_int8bin(bFile, NULL, maxDeg);
-      auto [fd, Q] = parse_int8bin(qFile, NULL, maxDeg);
-      std::cout << "Base file size " << B.size() << std::endl;
-      std::cout << "Query file size " << Q.size() << std::endl;
-      answers = compute_groundtruth<int8_t>(B, Q, k, D);
+      answers = compute_groundtruth<PointRange<uint8_t, Mips_Point<uint8_t>>>(B, Q, k);
     }
-    write_ibin(answers, std::string(gFile), k);
+  }else if(tp == "int8"){
+    std::cout << "Detected int8 coordinates" << std::endl;
+    if(df == "Euclidian"){
+      PointRange<int8_t, Euclidian_Point<int8_t>> B = PointRange<int8_t, Euclidian_Point<int8_t>>(bFile);
+      PointRange<int8_t, Euclidian_Point<int8_t>> Q = PointRange<int8_t, Euclidian_Point<int8_t>>(qFile);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      answers = compute_groundtruth<PointRange<int8_t, Euclidian_Point<int8_t>>>(B, Q, k);
+    } else if(df == "mips"){
+      PointRange<int8_t, Mips_Point<int8_t>> B = PointRange<int8_t, Mips_Point<int8_t>>(bFile);
+      PointRange<int8_t, Mips_Point<int8_t>> Q = PointRange<int8_t, Mips_Point<int8_t>>(qFile);
+      std::cout << "Base file size " << B.size() << std::endl;
+      std::cout << "Query file size " << Q.size() << std::endl;
+      answers = compute_groundtruth<PointRange<int8_t, Mips_Point<int8_t>>>(B, Q, k);
+    }
   }
+  write_ibin(answers, std::string(gFile), k);
 
   return 0;
 }
 
-
-// int main(int argc, char* argv[]) {
-//   if (argc != 8) {
-//     std::cout << "usage: compute_groundtruth <base> <query> <filetype> <vectype> <k> <df> <oFile>" << std::endl;
-//     return 1;
-//   }
-//   int k = std::atoi(argv[5]);
-
-//   std::string df = std::string(argv[6]);
-//   Distance* D;
-//   if(df == "Euclidian") D = new Euclidian_Distance();
-//   else if(df == "mips") D = new Mips_Distance();
-//   else{
-//     std::cout << "Error: invalid distance type" << std::endl;
-//     abort();
-//   }
-
-//   std::string ft = std::string(argv[3]);
-//   std::string tp = std::string(argv[4]);
-
-//   std::cout << "Computing the " << k << " nearest neighbors" << std::endl;
-//   if((ft != "bin") && (ft != "vec")){
-//     std::cout << "Error: file type not specified correctly, specify bin or vec" << std::endl;
-//     abort();
-//   }
-
-//   if((tp != "uint8") && (tp != "int8") && (tp != "float")){
-//     std::cout << "Error: vector type not specified correctly, specify int8, uint8, or float" << std::endl;
-//     abort();
-//   }
-
-//   if((ft == "vec") && (tp == "int8")){
-//     std::cout << "Error: incompatible file and vector types" << std::endl;
-//     abort();
-//   }
-
-//   int maxDeg = 0;
-
-//   parlay::sequence<parlay::sequence<pid>> answers;
-
-//   if(ft == "vec"){
-//     if(tp == "float"){
-//       std::cout << "Detected float coordinates" << std::endl;
-//       auto [md, B] = parse_fvecs(argv[1], NULL, maxDeg);
-//       auto [fd, Q] = parse_fvecs(argv[2], NULL, maxDeg);
-//       std::cout << "Base file size " << B.size() << std::endl;
-//       std::cout << "Query file size " << Q.size() << std::endl;
-//       answers = compute_groundtruth<float>(B, Q, k, D);
-//     }else if(tp == "uint8"){
-//       std::cout << "Detected uint8 coordinates" << std::endl;
-//       auto [md, B] = parse_bvecs(argv[1], NULL, maxDeg);
-//       auto [fd, Q] = parse_bvecs(argv[2], NULL, maxDeg);
-//       std::cout << "Base file size " << B.size() << std::endl;
-//       std::cout << "Query file size " << Q.size() << std::endl;
-//       answers = compute_groundtruth<uint8_t>(B, Q, k, D);
-//     }
-//     write_ivecs(answers, std::string(argv[7]), k);
-//   } else if(ft == "bin"){
-//     if(tp == "float"){
-//       std::cout << "Detected float coordinates" << std::endl;
-//       auto [md, B] = parse_fbin(argv[1], NULL, maxDeg);
-//       auto [fd, Q] = parse_fbin(argv[2], NULL, maxDeg);
-//       std::cout << "Base file size " << B.size() << std::endl;
-//       std::cout << "Query file size " << Q.size() << std::endl;
-//       answers = compute_groundtruth<float>(B, Q, k, D);
-//     }else if(tp == "uint8"){
-//       std::cout << "Detected uint8 coordinates" << std::endl;
-//       auto [md, B] = parse_uint8bin(argv[1], NULL, maxDeg);
-//       auto [fd, Q] = parse_uint8bin(argv[2], NULL, maxDeg);
-//       std::cout << "Base file size " << B.size() << std::endl;
-//       std::cout << "Query file size " << Q.size() << std::endl;
-//       answers = compute_groundtruth<uint8_t>(B, Q, k, D);
-//     }else if(tp == "int8"){
-//       std::cout << "Detected int8 coordinates" << std::endl;
-//       auto [md, B] = parse_int8bin(argv[1], NULL, maxDeg);
-//       auto [fd, Q] = parse_int8bin(argv[2], NULL, maxDeg);
-//       std::cout << "Base file size " << B.size() << std::endl;
-//       std::cout << "Query file size " << Q.size() << std::endl;
-//       answers = compute_groundtruth<int8_t>(B, Q, k, D);
-//     }
-//     write_ibin(answers, std::string(argv[7]), k);
-//   }
-
-  
-
-//   return 0;
-// }
