8137f8c08ea54cd4d0312d2857d8934c2ce21f94
Ben Landrum <8enlandrum@gmail.com>
2023-10-29 19:28:35 -0400

    added compile_prod.sh

---
+++ python/compile_prod.sh
@@ -0,0 +1,3 @@
+# g++ -DSTATS -DHOMEGROWN -pthread -mcx16 -O3 -Wall -shared -std=c++17 -march=native -DNDEBUG -I . -fPIC $(python3 -m pybind11 --includes) vamana_index.cpp -o vamana_index$(python3-config --extension-suffix) -DHOMEGROWN -pthread -ldl -L/usr/local/lib -ljemalloc 
+
+g++ -DSTATS -DHOMEGROWN -pthread -mcx16 -O3 -Wall -shared -std=c++17 -march=native -DNDEBUG -I . -fPIC $(python3 -m pybind11 --includes) module.cpp -o _ParlayANNpy$(python3-config --extension-suffix) -DHOMEGROWN -pthread -ldl -L/usr/local/lib -w #-DVERBOSE #-ljemalloc 
