ee1dc629f6cc76359d432f7071bf3fd571d55c54
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-11-13 16:36:59 -0500

    bug in arguments to batch search

--- python/graph_index.cpp
+++ python/graph_index.cpp
@@ -53,7 +53,7 @@ struct GraphIndex{
     }
 
     NeighborsAndDistances batch_search(py::array_t<T, py::array::c_style | py::array::forcecast> &queries, uint64_t num_queries, uint64_t knn,
-                        uint64_t beam_width, uint64_t visit_limit = -1){
+                        uint64_t beam_width, int64_t visit_limit = -1){
         if(visit_limit == -1) visit_limit = G.size();
         QueryParams QP(knn, beam_width, 1.35, visit_limit, G.max_degree());
 
