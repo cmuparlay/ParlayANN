6219977614d9100e8b492caf3684c1f54dfde19a
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2024-04-02 10:54:09 -0400

    added testing infrastructure

---
+++ tests/datastructures/rand_r_32.h
@@ -0,0 +1,80 @@
+/* Reentrant random function frm POSIX.1c.
+   Copyright (C) 1996, 1999 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#ifndef RAND_R_32_H__
+#define RAND_R_32_H__
+
+/* This algorithm is mentioned in the ISO C standard, here extended
+   for 32 bits.  */
+
+/**
+ *  Changes:
+ *
+ *    The file/function name has been changed from rand_r to rand_r_32.
+ *
+ *    Added attributes for transactional use.
+ *
+ *    Cloned the function to handle the case where the seed is volatile
+ */
+inline int
+rand_r_32 (unsigned int *seed)
+{
+    unsigned int next = *seed;
+    int result;
+
+    next *= 1103515245;
+    next += 12345;
+    result = (unsigned int) (next / 65536) % 2048;
+
+    next *= 1103515245;
+    next += 12345;
+    result <<= 10;
+    result ^= (unsigned int) (next / 65536) % 1024;
+
+    next *= 1103515245;
+    next += 12345;
+    result <<= 10;
+    result ^= (unsigned int) (next / 65536) % 1024;
+
+    *seed = next;
+
+    return result;
+}
+
+namespace my_rand {
+  thread_local static unsigned long x=123456789, y=362436069, z=521288629;
+
+  void init(int seed) {
+    x += seed;
+  }
+
+  unsigned long get_rand(void) {          //period 2^96-1
+    unsigned long t;
+    x ^= x << 16;
+    x ^= x >> 5;
+    x ^= x << 1;
+
+    t = x;
+    x = y;
+    y = z;
+    z = t ^ x ^ y;
+
+    return z;
+  }
+}
+
+#endif // RAND_R_32_H__
