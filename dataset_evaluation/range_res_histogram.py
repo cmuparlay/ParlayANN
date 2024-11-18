899c2ca95e0440e93b86a40af3b3567410c61543
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2024-11-18 16:26:08 -0500

    update plotting code

--- dataset_evaluation/range_res_histogram.py
+++ dataset_evaluation/range_res_histogram.py
@@ -2,9 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 p="/ssd1/data/"
-filenames = ['FB_ssnpp/ssnpp-1M', 'bigann/range_gt_1M_10000','MSTuringANNS/range_gt_1M_100K_.3', 'gist/range_gt_1M_.5', 'deep1b/range_gt_1M_.02','msmarco_websearch/range_gt_1M_-62', 'wikipedia_cohere/range_gt_1M_-10.5', 'OpenAIArXiv/openai_gt_1M_.2']
-titles = ['SSNPP', 'BIGANN', 'MSTuring', 'GIST', 'DEEP', 'MSMARCO', 'Wikipedia', 'OpenAI']
+filenames = ['FB_ssnpp/ssnpp-1M', 'bigann/range_gt_1M_10000','MSTuringANNS/range_gt_1M_100K_.3', 'gist/range_gt_1M_.5', 'deep1b/range_gt_1M_.02','msmarco_websearch/range_gt_1M_-62', 'wikipedia_cohere/range_gt_1M_-10.5', 'OpenAIArXiv/openai_gt_1M_.2', 'text2image1B/range_gt_1M_-.6']
+titles = ['SSNPP', 'BIGANN', 'MSTuring', 'GIST', 'DEEP', 'MSMARCO', 'Wikipedia', 'OpenAI', 'Text2Image']
 result_dir="graphs/range_result_histograms/"
+
+dsinfo = {
+  "BIGANN" : {"mult": 100, 
+                "color": "C0"},
+  "MSTuring" : {"mult": 1000,  
+                "color": "C1"},
+  "DEEP" : {"mult": 100, 
+                "color": "C2"},
+  "GIST" : {"mult": 10,  
+                "color": "C3"},
+  "SSNPP" : {"mult": 1000,  
+                "color": "C4"},
+  "Wikipedia" : {"mult": 100, 
+                "color": "C5"},
+  "MSMARCO" : {"mult": 100, 
+                "color": "C6"},
+  "Text2Image" : {"mult": 1000, 
+                "color": "C7"},
+  "OpenAI" : {"mult": 100,  
+                "color": "C8"},
+}
 
 def plot_distances_hist(filename, title):
     num_results = np.fromfile(p+filename, dtype=np.int32, count=1)[0]
@@ -16,7 +37,7 @@ def plot_distances_hist(filename, title):
     plt.ylabel("Frequency")
     plt.title(title)
     plt.yscale("log")
-    plt.hist(result, bins=int(len(result)/100), alpha=1.0)
+    plt.hist(result, bins=int(len(result)/dsinfo[title]["mult"]), alpha=1.0, color=dsinfo[title]["color"])
     plt.savefig(result_dir + title + ".pdf")
     plt.close()
 
