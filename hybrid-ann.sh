0d8807f973b33d854362bb6da5444d7b56e2dc38
ravishankar <rakri@microsoft.com>
2024-12-23 05:40:03 -0800

    Exp: add sample runs

---
+++ hybrid-ann.sh
@@ -0,0 +1,5 @@
+cd algorithms/vamana
+make
+# ./neighbors -R 32 -L 50 -alpha 1.2 -graph_outfile /nvmessd1/fbv4/avarhade/parlay_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path parlay_vamana_res.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
+
+./neighbors -R 32 -L 50 -k 25 -alpha 1.2 -graph_path /nvmessd1/fbv4/avarhade/parlay_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path parlay_vamana_res.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
