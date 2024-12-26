3514db1bc2164e626ce89e54f6800d862a7a5ebf
ravishankar <rakri@microsoft.com>
2024-12-25 20:30:55 -0800

    update command

--- hybrid-ann.sh
+++ hybrid-ann.sh
@@ -1,5 +1,9 @@
-cd algorithms/vamana
-make
+# cd algorithms/vamana
+# make
 # ./neighbors -R 32 -L 50 -alpha 1.2 -graph_outfile /nvmessd1/fbv4/avarhade/parlay_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path parlay_vamana_res.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
+# ./neighbors -R 32 -L 50 -k 25 -alpha 1.2 -graph_path /nvmessd1/fbv4/avarhade/parlay_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path results.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
 
-./neighbors -R 32 -L 50 -k 25 -alpha 1.2 -graph_path /nvmessd1/fbv4/avarhade/parlay_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path results.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
+cd algorithms/HNSW
+make 
+
+./neighbors -m 32 -efc 50 -alpha 1.2 -ml 0.0 -graph_outfile /nvmessd1/fbv4/avarhade/parlay_hnsw_prec1M_32_50 -query_path /nvmessd1/fbv4/queries.fbin -gt_path /nvmessd1/fbv4/prec1M_gt100.bin -res_path hnsw_results.csv -data_type float -dist_func Euclidian -base_path /nvmessd1/fbv4/prec1M.fbin
