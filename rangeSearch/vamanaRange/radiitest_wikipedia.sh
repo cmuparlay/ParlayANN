4d62c87389aad670024284378a7a144dfb8646a6
Taekseung Kim <taekseuk@andrew.cmu.edu>
2025-08-04 01:23:35 -0400

    update to absolute value difference, with various recall cuts

--- rangeSearch/vamanaRange/radiitest_wikipedia.sh
+++ rangeSearch/vamanaRange/radiitest_wikipedia.sh
@@ -11,6 +11,7 @@ MATCHES[-12.0]=177
 MATCHES[-12.5]=65
 
 recalls=(0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 0.99)
+#recalls=(0.2 0.4 0.5)
 
 for recall_cut in "${recalls[@]}"; do
   CSV_FILE="qps_vs_matches_recall_wikipedia_${recall_cut}.csv"
@@ -24,20 +25,30 @@ for recall_cut in "${recalls[@]}"; do
     filename=$(basename "$file")
     algo=$(echo "$filename" | awk -F'_' '{print $(NF-1)}')
     radius=$(echo "$filename" | awk -F'_' '{print $NF}' | sed 's/.out//')
-    
+
+    best_diff=1000
+    best_beam=""
+    best_qps=""
+
     while IFS= read -r line; do
       [[ "$line" == For\ Beam:* ]] || continue
 
       recall=$(echo "$line" | awk -F'Cum Recall=' '{print $2}' | awk -F',' '{print $1}' | tr -d ' ')
       [[ ! "$recall" =~ ^[0-9.]+$ ]] && continue
 
-      if (( $(echo "$recall > $recall_cut" | bc -l) )); then
-        beam=$(echo "$line" | awk -F'[:,]' '{print $2}' | tr -d ' ')
-        qps=$(echo "$line" | awk -F'QPS=' '{print $2}' | awk -F',' '{print $1}' | tr -d ' ')
-        echo "$radius,${MATCHES[$radius]},$qps,$beam,$algo" >> "$CSV_FILE"
-        break
+      diff=$(echo "$recall - $recall_cut" | bc -l)
+      abs_diff=$(echo "$diff" | awk '{print ($1 >= 0) ? $1 : -$1}')
+
+      if (( $(echo "$abs_diff < $best_diff" | bc -l) )); then
+        best_diff="$abs_diff"
+        best_beam=$(echo "$line" | awk -F'[:,]' '{print $2}' | tr -d ' ')
+        best_qps=$(echo "$line" | awk -F'QPS=' '{print $2}' | awk -F',' '{print $1}' | tr -d ' ')
       fi
     done < "$file"
+
+    if [[ -n "$best_beam" && -n "$best_qps" ]]; then
+      echo "$radius,${MATCHES[$radius]},$best_qps,$best_beam,$algo" >> "$CSV_FILE"
+    fi
   done
 
   python3 radiitest_pscript_wikipedia.py "$CSV_FILE" "$recall_cut"
